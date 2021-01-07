530dddae4cc6d326e8b8b6dc358491287f88447c
soohyunnn <sksmsqodn20@gmail.com>
2021-01-07 20:22:27 +0900

    Initial commit

---
+++ src/main/java/dang/gun/com/DangGunBackApplication.java
@@ -0,0 +1,13 @@
+package dang.gun.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangGunBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangGunBackApplication.class, args);
+	}
+
+}
