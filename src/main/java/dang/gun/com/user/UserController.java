24773b92054d3078c61d9840dde2212d6d9f5f6b
soohyunnn <58289675+soohyunnn@users.noreply.github.com>
2021-01-10 21:10:38 +0900

    중복회원조회 API

--- src/main/java/dang/gun/com/user/UserController.java
+++ src/main/java/dang/gun/com/user/UserController.java
@@ -1,13 +1,15 @@
 package dang.gun.com.user;
 
+import org.json.simple.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
 
     private final UserService userService;
@@ -30,5 +32,49 @@ public class UserController {
         //클라이언트 정보 - ip 주소 반환
         return ResponseEntity.ok(request.getRemoteAddr());
     }
+
+    /**
+     * 중복회원조회
+     * @param email
+     * @return
+     */
+    @ResponseBody
+    @GetMapping("/user/selectedUser")
+     public JSONObject getByEmail(@RequestParam(value="email") String email) {
+        System.out.println("UserController.getByEmail");
+        System.out.println("email = " + email);
+        JSONObject json = new JSONObject();
+        try{
+            List<User> user = userService.getByEmail(email);
+            System.out.println("user = " + user);
+            json.put("result", "SUCCESS");
+            json.put("data", user);
+            return json;
+        } catch (Exception e){
+            e.printStackTrace();
+            json.put("result", "FAIL");
+            return json;
+        }
+     }
+
+     @ResponseBody
+     @GetMapping("/users")
+     public JSONObject hello(){
+        JSONObject json = new JSONObject();
+
+        try{
+            List<User> users = userService.findAll();
+            System.out.println("user = " + users);
+            json.put("result", "SUCCESS");
+            json.put("data", users);
+            return json;
+        } catch (Exception e){
+            e.printStackTrace();
+            json.put("result", "FAIL");
+            return json;
+        }
+
+
+     }
 
 }
--- src/main/java/dang/gun/com/user/UserRepository.java
+++ src/main/java/dang/gun/com/user/UserRepository.java
@@ -1,8 +1,14 @@
 package dang.gun.com.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    public List<User> findAllByEmail(String email);
 }
