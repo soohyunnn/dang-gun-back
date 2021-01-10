a55632677a200a756f257c8e65648e15751356f2
soohyunnn <58289675+soohyunnn@users.noreply.github.com>
2021-01-10 21:10:38 +0900

    중복회원조회 API

--- src/main/java/dang/gun/com/user/UserService.java
+++ src/main/java/dang/gun/com/user/UserService.java
@@ -2,6 +2,8 @@ package dang.gun.com.user;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -14,6 +16,16 @@ public class UserService {
     public int save(User user){
         userRepository.save(user);
         return user.getId();
+    }
+
+    public List<User> getByEmail(String email){
+        System.out.println("email = " + email);
+        System.out.println("aaa::"+userRepository.findAllByEmail(email));
+        return userRepository.findAllByEmail(email);
+    }
+
+    public List<User> findAll(){
+        return userRepository.findAll();
     }
 
 }
